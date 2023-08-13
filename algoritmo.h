1650f2bcd10895fc5f824985a62c1be9568358a2
RazorLazor27 <nicolas.berenguela@gmail.com>
2023-08-13 02:37:53 -0400

    No funciona el ordenamiento por carpeta todavia

--- algoritmo.h
+++ algoritmo.h
@@ -8,8 +8,6 @@ typedef struct {
     char tipo[15];
 } InfoLinea;
 
-//InfoLinea contador_general(const char *nombre_archivo);
-
 void contador_general2(InfoLinea * inf);
 
 void horizontal(InfoLinea * infosuck);
@@ -17,7 +15,5 @@ int horizontal_aux(char * nombre, char * str);
 
 InfoLinea * inicial();
 
-char matriz(char * nombre_archivo);
-void vertical(char nom[], const char nomb);
 
 #endif
